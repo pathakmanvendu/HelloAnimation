3fd3a02de7d5ae68498b7ac78836ae86d8233110
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-14 21:47:15 +0530

    Initial Commit

---
+++ HelloAnimation/HelloAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAnimationApp.swift
+//  HelloAnimation
+//
+//  Created by Manvendu Pathak on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
